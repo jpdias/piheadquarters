6cea68a2d9fe00ddd2410e08b8fb002a934c1c92
jpdias <jpdias@live.com.pt>
2017-09-21 11:53:50 +0100

    addd subscribe

--- arduino/src/main.ino
+++ arduino/src/main.ino
@@ -4,12 +4,10 @@
 #include "DHT.h"
 
 #define DHTPIN 12 // what digital pin we're connected to NodeMCU (D6)
+#define LEDPIN D0
 
-// Uncomment whatever type you're using!
-#define DHTTYPE DHT11 // DHT 11
+#define DHTTYPE DHT22
 
-//#define DHTTYPE DHT22 // DHT 22 (AM2302), AM2321
-//#define DHTTYPE DHT21 // DHT 21 (AM2301)
 // Connect pin 1 (on the left) of the sensor to +5V
 // NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
 // to 3.3V instead of 5V!
@@ -21,6 +19,7 @@
 // tweak the timings for faster processors. This parameter is no longer needed
 // as the current DHT reading algorithm adjusts itself to work on faster procs.
 
+// Code based on https://github.com/titusece/ESP8266-MQTT-Publish-DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -34,21 +33,25 @@ DHT dht(DHTPIN, DHTTYPE);
 #define humidity_topic "sensor/humidity"
 #define temperature_celsius_topic "sensor/temperature_celsius"
 #define temperature_fahrenheit_topic "sensor/temperature_fahrenheit"
+#define actuator_led_status "actuator/led_status"
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   dht.begin();
   setup_wifi();
   client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 
-String macToStr(const uint8_t* mac)
+String macToStr(const uint8_t *mac)
 {
   String result;
-  for (int i = 0; i < 6; ++i) {
+  for (int i = 0; i < 6; ++i)
+  {
     result += String(mac[i], 16);
     if (i < 5)
       result += ':';
@@ -56,8 +59,8 @@ String macToStr(const uint8_t* mac)
   return result;
 }
 
-
-void setup_wifi() {
+void setup_wifi()
+{
   delay(10);
   // We start by connecting to a WiFi network
   Serial.println();
@@ -66,7 +69,8 @@ void setup_wifi() {
 
   WiFi.begin(wifi_ssid, wifi_password);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -77,31 +81,36 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-void reconnect() {
+void reconnect()
+{
   // Loop until we're reconnected
-  while (!client.connected()) {
-        Serial.print("Attempting MQTT connection...");
-    
-          // Generate client name based on MAC address and last 8 bits of microsecond counter
-      String clientName;  
-      clientName += "esp8266-";
-      uint8_t mac[6];
-      WiFi.macAddress(mac);
-      clientName += macToStr(mac);
-      clientName += "-";
-      clientName += String(micros() & 0xff, 16);
-      Serial.print("Connecting to ");
-      Serial.print(mqtt_server);
-      Serial.print(" as ");
-      Serial.println(clientName);
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
 
+    // Generate client name based on MAC address and last 8 bits of microsecond counter
+    String clientName;
+    clientName += "esp8266-";
+    uint8_t mac[6];
+    WiFi.macAddress(mac);
+    clientName += macToStr(mac);
+    clientName += "-";
+    clientName += String(micros() & 0xff, 16);
+    Serial.print("Connecting to ");
+    Serial.print(mqtt_server);
+    Serial.print(" as ");
+    Serial.println(clientName);
 
     // Attempt to connect
     // If you do not want to use a username and password, change next line to
-  if (client.connect((char*) clientName.c_str())) {
-    //if (client.connect((char*) clientName.c_str()), mqtt_user, mqtt_password)) {
+    if (client.connect((char *)clientName.c_str()))
+    {
+      //if (client.connect((char*) clientName.c_str()), mqtt_user, mqtt_password)) {
+      client.subscribe(actuator_led_status);
       Serial.println("connected");
-    } else {
+    }
+    else
+    {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
@@ -111,61 +120,76 @@ void reconnect() {
   }
 }
 
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+   char receivedChar = (char)payload[i];
+   Serial.print(receivedChar);
+   if (receivedChar == '0')
+   // ESP8266 Huzzah outputs are "reversed"
+   digitalWrite(ledPin, HIGH);
+   if (receivedChar == '1')
+    digitalWrite(ledPin, LOW);
+   }
+   Serial.println();
+ }
 
-void loop() {
-  
-      if (!client.connected()) {
-        reconnect();
-      }
-      client.loop();
+void loop()
+{
 
-      // Wait a few seconds between measurements.
-      delay(2000);
-      
-      // Reading temperature or humidity takes about 250 milliseconds!
-      // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-      float h = dht.readHumidity();
-      // Read temperature as Celsius (the default)
-      float t = dht.readTemperature();
-      // Read temperature as Fahrenheit (isFahrenheit = true)
-      float f = dht.readTemperature(true);
-      
-      // Check if any reads failed and exit early (to try again).
-      if (isnan(h) || isnan(t) || isnan(f)) {
-      Serial.println("Failed to read from DHT sensor!");
-      return;
-      }
-      
-      // Compute heat index in Fahrenheit (the default)
-      float hif = dht.computeHeatIndex(f, h);
-      // Compute heat index in Celsius (isFahreheit = false)
-      float hic = dht.computeHeatIndex(t, h, false);
-      Serial.print("Humidity: ");
-      Serial.print(h);
-      Serial.print(" %\t");
-      Serial.print("Temperature: ");
-      Serial.print(t);
-      Serial.print(" *C ");
-      Serial.print(f);
-      Serial.print(" *F\t");
-      Serial.print("Heat index: ");
-      Serial.print(hic);
-      Serial.print(" *C ");
-      Serial.print(hif);
-      Serial.println(" *F");
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
 
+  // Wait a few seconds between measurements.
+  delay(2000);
 
-      Serial.print("Temperature in Celsius:");
-      Serial.println(String(t).c_str());
-      client.publish(temperature_celsius_topic, String(t).c_str(), true);
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
 
-      Serial.print("Temperature in Fahrenheit:");
-      Serial.println(String(f).c_str());
-      client.publish(temperature_fahrenheit_topic, String(f).c_str(), true);
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f))
+  {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
 
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
+  Serial.println(" *F");
 
-      Serial.print("Humidity:");
-      Serial.println(String(h).c_str());
-      client.publish(humidity_topic, String(h).c_str(), true);
+  Serial.print("Temperature in Celsius:");
+  Serial.println(String(t).c_str());
+  client.publish(temperature_celsius_topic, String(t).c_str(), true);
+
+  Serial.print("Temperature in Fahrenheit:");
+  Serial.println(String(f).c_str());
+  client.publish(temperature_fahrenheit_topic, String(f).c_str(), true);
+
+  Serial.print("Humidity:");
+  Serial.println(String(h).c_str());
+  client.publish(humidity_topic, String(h).c_str(), true);
 }
-
